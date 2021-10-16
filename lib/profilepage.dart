73ac320720d54474b4888f2519f4e5929a8c15e1
nayan0020 <shaikh.noyne12@gmail.com>
2021-10-16 11:23:37 +0600

    ÃAdd a post button

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -219,8 +219,43 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
               ),
               Container(
+                height: MediaQuery.of(context).size.height/7,
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 17),
+                child: Wrap(
+                  children:[
+                Container(
+                  height: MediaQuery.of(context).size.height/15,
+                  width: MediaQuery.of(context).size.width/4,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 250, 250, 1),
+                  border: Border.all(
+                    color: Color.fromRGBO(31, 31, 31, 1),
+                    width: 3,
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                child: FlatButton.icon(
+                    onPressed: (){},
+                  icon: Icon(
+                    Icons.post_add_sharp,
+                    size: 15,
+                    color: Color.fromRGBO(21, 21, 21, 1),
+                  ),
+                  label: Text (
+                    "Posts",
+                    style: TextStyle(
+                      fontWeight:FontWeight.w500,
+                      fontSize: 15,
+                      color: Color.fromRGBO(21, 21, 21, 1),
+                    ),
+                  ),
+                ),
 
               ),
+                   ],
+                 ),
+                 ),
             ],
           ),
         ),
